4201aeb43a127eb2caeee1984f03576a96769e46
Elena <ea.alipova80@gmail.com>
2022-11-28 20:35:32 +0300

    add program code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите элементы массива: ");
+string[] array = new string[size];
+
+for (int i=0; i,size; i++){
+    array[i] = Console.ReadLine()!;
+}
+string[] newArray = new string[size];
+
+massive(size);
+Console.WriteLine("Вывщд массива:");
+for (int i=0; i<size; i++){
+    if (newArray[i] is not null){
+        Console.Write($"{newArray[i]},");
+    }
+}
+void massive(int size){
+    for (int i=0; i<size; i++){
+        if (array[i].Lenght<=3){
+            newArray[i] = array[i];
+        }
+    }
+}
